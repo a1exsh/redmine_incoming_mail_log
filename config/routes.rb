3e2c9de4afe7cfb47a4f4a26d2394c832da67eef
Alex Shulgin <ash@commandprompt.com>
2011-08-15 20:25:25 +0300

    Limited routes to actions only implemented in controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :incoming_mails
+  map.resources :incoming_mails, :only => [:index, :show, :destroy]
 #  map.resources :incoming_mails, :path_prefix => '/projects/:project_id'
 end
