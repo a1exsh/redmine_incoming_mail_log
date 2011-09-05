ed14de488c2020ef65f60b1d86de4ed0ba0795a9
Alex Shulgin <ash@commandprompt.com>
2011-09-05 19:34:10 +0300

    Added mail details to failed submission notification.

--- lib/redmine_incoming_mail_log/mailer_patch.rb
+++ lib/redmine_incoming_mail_log/mailer_patch.rb
@@ -1,9 +1,15 @@
 module RedmineIncomingMailLog
   module MailerPatch
+    def self.included(base)
+      base.class_eval do
+        helper IncomingMailsHelper
+      end
+    end
+
     def failed_incoming_mail(incoming_mail, notify_addresses)
       recipients notify_addresses
       subject l(:mail_subject_failed_incoming_mail)
-      body :incoming_mail => incoming_mail,
+      body :mail => incoming_mail,
         :url => incoming_mail_url(incoming_mail)
       render_multipart('failed_incoming_mail', body)
     end
