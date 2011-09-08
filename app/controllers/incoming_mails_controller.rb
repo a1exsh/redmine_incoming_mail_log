a3fc6dfebfa0f85cbe57bb343b45be6901712604
Alex Shulgin <ash@commandprompt.com>
2011-09-08 21:57:21 +0300

    Added filter by project to the index page.

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -6,6 +6,17 @@ class IncomingMailsController < ApplicationController
   before_filter :find_incoming_mail, :only => [:show, :destroy]
 
   def index
+    conditions = {}
+
+    @projects = Project.all
+    if params[:project].present?
+      begin
+        @project = Project.find(params[:project]) 
+        conditions.merge!(:target_project => @project.identifier)
+      rescue ActiveRecord::RecordNotFound
+      end
+    end
+
     @limit = per_page_option
 
     @mail_count = IncomingMail.count
@@ -13,7 +24,8 @@ class IncomingMailsController < ApplicationController
     @offset ||= @mail_pages.current.offset
 
     # TODO: search
-    @mails = IncomingMail.all(:order => "created_on DESC",
+    @mails = IncomingMail.all(:conditions => conditions,
+                              :order => "created_on DESC",
                               :offset => @offset,
                               :limit => @limit)
   end
