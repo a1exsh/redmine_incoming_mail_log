d2050852bfe12e90b231309561ad59fc1584f6f8
Alex Shulgin <ash@commandprompt.com>
2013-02-21 14:24:28 -0800

    Fix extra slipped comma.

--- lib/redmine_incoming_mail_log/mailer_patch.rb
+++ lib/redmine_incoming_mail_log/mailer_patch.rb
@@ -7,7 +7,7 @@ module RedmineIncomingMailLog
     end
 
     def failed_incoming_mail(incoming_mail, notify_addresses)
-      @mail = incoming_mail,
+      @mail = incoming_mail
       @url = incoming_mail_url(incoming_mail)
       mail :to => notify_addresses,
         :subject => l(:mail_subject_failed_incoming_mail)
